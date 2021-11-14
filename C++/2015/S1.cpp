5b7656ac272b50f3dc2b0af044ea1154aa863582
CallMeTwitch <63736686+CallMeTwitch@users.noreply.github.com>
2021-11-14 10:09:43 -0330

    Add files via upload

--- C++/2015/S1.cpp
+++ C++/2015/S1.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    int n, k;cin >> k;
+    vector<int> mem;
+
+    for (int q=0;q<k;q++) {
+        cin >> n;
+        if (n == 0) {
+            mem.pop_back();
+        }
+        else mem.push_back(n);
+    }
+    n = 0;
+    for (int q :mem) {
+        n += q;
+    }
+    cout << n;
+}
