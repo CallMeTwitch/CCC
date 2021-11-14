cd5bb4f9486df37e4d60ff445946f50eed570d85
CallMeTwitch <63736686+CallMeTwitch@users.noreply.github.com>
2021-11-14 10:07:48 -0330

    Add files via upload

--- C++/2018/S1.cpp
+++ C++/2018/S1.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    int n;cin >> n;
+    double v[n];
+    double m = 2000000000;
+
+    for (int q=0;q<n;q++) cin >> v[q];
+    sort(v, v+n);
+    for (int q=1;q+1<n;q++) {
+        m = min(m, ((v[q] - v[q - 1]) / 2) + ((v[q + 1] - v[q]) / 2));
+    }
+
+    cout << fixed << setprecision(1) << m;
+}
