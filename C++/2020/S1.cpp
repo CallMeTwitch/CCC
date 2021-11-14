4fd1258d0c2faaea32607da4b1dafe899778414d
CallMeTwitch <63736686+CallMeTwitch@users.noreply.github.com>
2021-11-14 10:06:50 -0330

    Add files via upload

--- C++/2020/S1.cpp
+++ C++/2020/S1.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    int time, pos, n;cin >> n;
+    map<int, int> speeds;
+    double m = 0.0;
+
+    for (int q=0;q<n;q++) {
+        cin >> time >> pos;
+        speeds[time] = pos;
+    }
+    for (auto q = speeds.begin();next(q)!=speeds.end();q++) {
+        m = max(m, abs(double(next(q)->second - q->second) / double(next(q)->first - q->first)));
+    }
+    cout << fixed << setprecision(1) << m;
+}
